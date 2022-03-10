7343b2a2f72e4ec095b9eb4bf4a6dc9161cca331
Leonardo Oliveira <leozartino@gmail.com>
2022-03-10 20:08:07 -0300

    feat: add course create dto

---
+++ GerenciadorDeCursos/Dtos/CourseCreateDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GerenciadorDeCursos.Dtos
+{
+    public class CourseCreateDTO
+    {
+        public string Title { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Status { get; set; }
+    }
+}
