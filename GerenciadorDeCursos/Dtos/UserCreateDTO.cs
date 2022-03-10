37fa480917ec1de25fa79762571cb192de9645d3
Leonardo Oliveira <leozartino@gmail.com>
2022-03-10 16:08:41 -0300

    feat: add userCreateDTO

---
+++ GerenciadorDeCursos/Dtos/UserCreateDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GerenciadorDeCursos.Dtos
+{
+    public class UserCreateDTO
+    {
+        [Required(ErrorMessage = "Please, insert user username!")]
+        public string Username { get; set; }
+        [Required(ErrorMessage = "Please, insert user password!")]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "Please, insert user role!")]
+        public string Role { get; set; }
+
+    }
+}
