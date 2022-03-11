274b004a2e25cc6b6929df46b0f2a2e5a5c8a4fc
Leonardo Oliveira <leozartino@gmail.com>
2022-03-10 22:56:12 -0300

    feat: add datacontext injection

--- GerenciadorDeCursos/Startup.cs
+++ GerenciadorDeCursos/Startup.cs
@@ -1,18 +1,15 @@
+using GerenciadorDeCursos.Data;
 using GerenciadorDeCursos.Interfaces;
 using GerenciadorDeCursos.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace GerenciadorDeCursos
 {
@@ -67,10 +64,13 @@ namespace GerenciadorDeCursos
                     }
                 });
             });
+
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
         }
 
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
