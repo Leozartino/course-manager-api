6a2644b3350df443745dfcf5da9209b8a344c454
Leonardo Oliveira <leozartino@gmail.com>
2022-03-11 23:10:44 -0300

    feat: add basics endpoints for courses

--- GerenciadorDeCursos/Controllers/CoursesController.cs
+++ GerenciadorDeCursos/Controllers/CoursesController.cs
@@ -1,4 +1,5 @@
-﻿using GerenciadorDeCursos.Interfaces;
+﻿using GerenciadorDeCursos.Dtos;
+using GerenciadorDeCursos.Interfaces;
 using GerenciadorDeCursos.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -46,7 +47,7 @@ namespace GerenciadorDeCursos.Controllers
 
         }
 
-        //Quais usuarios podem consultar por ID?
+        [AllowAnonymous]
         [HttpGet]
         [Route("show/{id}")]
         public async Task<ActionResult<Course>> ShowCourse(Guid id)
@@ -62,7 +63,30 @@ namespace GerenciadorDeCursos.Controllers
 
         }
 
-        //Quais usuarios podem criar um curso?
+        //Quais usuarios podem criar um curso? Gerente e Secretario
+        [HttpPost]
+        [Route("post")]
+        public async Task<ActionResult<Course>> CreateCourse(CourseCreateDTO courseDTO)
+        {
+
+            Guid id = Guid.NewGuid();
+
+            Course course = new()
+            {
+                Id = id,
+                Title = courseDTO.Title,
+                StartDate = courseDTO.StartDate,
+                EndDate = courseDTO.EndDate,
+                Status = courseDTO.Status,
+                
+            };
+
+            var course_created = await _courseRepository.CreateCourseAsync(course);
+            // var days = (courseDTO.EndDate - courseDTO.StartDate).Days;
+
+            return Created("~api/courses/post", course_created);
+        }
+
         //Quais usuarios podem atualizar dados de um curso além do status?
     }
 }
