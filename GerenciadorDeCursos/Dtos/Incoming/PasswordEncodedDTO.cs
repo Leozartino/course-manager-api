5f55c94bc82436583ff70d4ac2c6ed3217b3c9c1
Leonardo Oliveira <leozartino@gmail.com>
2022-03-13 00:51:57 -0300

    feat: remove password dto unsed

--- GerenciadorDeCursos/Dtos/Incoming/PasswordEncodedDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace GerenciadorDeCursos.Dtos.Incoming
-{
-    public class PasswordEncodedDTO
-    {
-        public byte[] PasswordHash { get; set; }
-        public byte[] PasswordSalt { get; set; }
-    }
-}
